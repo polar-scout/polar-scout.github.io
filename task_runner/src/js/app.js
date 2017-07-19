ec1ecfc517f797fa839192319a00c75ac9556cb1
Polar Scout <polar-scout@users.noreply.github.com>
2017-07-19 19:31:33 +0100

    Test

--- task_runner/src/js/app.js
+++ task_runner/src/js/app.js
@@ -12,26 +12,65 @@
 
         console.log('mutha');
 
-        window.pAsyncInit = function() {
-            PDK.init({
-                appId: "4912419253518348031", // Change this
-                cookie: true
-            });
-        };
-
-        (function(d, s, id){
-            var js, pjs = d.getElementsByTagName(s)[0];
-            if (d.getElementById(id)) {return;}
-            js = d.createElement(s); js.id = id;
-            js.src = "//assets.pinterest.com/sdk/sdk.js";
-            pjs.parentNode.insertBefore(js, pjs);
-        }(document, 'script', 'pinterest-jssdk'));
-
-
-
     });
 
     // The rest of code goes here!
+
+    // Const
+    const PIN_APP   = '4912419253518348031',
+          PIN_SCOPE = 'read_public, write_public',
+          PIN_BTN   = document.getElementById('js-pin_btn');
+
+    // Initialize once with app id
+    PDK.init({
+        appId: PIN_APP,
+        cookie: true
+    });
+
+    var Login = {
+
+        /*
+         *  Set auth state and redirect if necessary
+         */
+        resetState: function() {
+            var state = {
+                pinterest: Pinterest.loggedIn()
+            };
+        },
+
+        /*
+         *  Login using Instagram OAuth
+         */
+        pinLogin: function() {
+            Pinterest.login(Login.resetState);
+        }
+
+    }
+
+    /*
+     *  Wrapper for all Pinterest SDK actions
+     */
+    var Pinterest = {
+
+        /*
+         *  Use the SDK to login to Pinterest
+         *  @param {Function} callback - function fired on completion
+         */
+        login: function(callback) {
+            PDK.login({ scope : PIN_SCOPE }, callback);
+        },
+
+        /*
+         *  Use DK to determine auth state of user
+         *  @returns {Boolean}
+         */
+        loggedIn: function() {
+            return !!PDK.getSession();
+        }
+
+    };
+
+    PIN_BTN.addEventListener('click', Login.pinLogin());
 
 }));
 
